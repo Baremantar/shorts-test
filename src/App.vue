797a3d11010f83de1c8c0703605f2e3601aae936
Baremantar <baremantar@gmail.com>
2024-02-20 14:14:49 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import Flicking from './components/Flicking.vue';
+</script>
+
+<template>
+  <main>
+    <div class="wrapper">
+      <Flicking/>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  main {
+    display: flex;
+    place-items: center;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  main .wrapper {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
---
+++ src/components/Flicking.vue
@@ -0,0 +1,57 @@
+<script>
+import Flicking from "@egjs/vue3-flicking";
+import vid1 from "../assets/video/aqua2.mp4";
+import vid2 from "../assets/video/attributes.mp4";
+import vid3 from "../assets/video/beach.mp4";
+import vid4 from "../assets/video/children-bar.mp4";
+import vid5 from "../assets/video/park.mp4";
+import vid6 from "../assets/video/WI-FI.mp4";
+import vid7 from "../assets/video/zoo.mp4";
+import {ref} from 'vue';
+
+
+const data = [vid1, vid2, vid3, vid4, vid5, vid6, vid7];
+const videoOptions = ref({
+    autoplay: false,
+    current: {}
+});
+
+export default {
+  components: {
+    Flicking,
+  },
+  data() {
+    return {
+      data,
+      videoOptions,
+    };
+  },
+};
+
+
+
+</script>
+
+<template>
+  <Flicking
+    :options="{ circular: true, horizontal: false }"
+    @move-end="e => {  }"
+    class="flicker-wrapper"
+  >
+    <video v-for="source in data" :key="source" v-bind:autoplay="videoOptions.autoplay">
+      <source :src="source" type="video/mp4" />
+    </video>
+  </Flicking>
+</template>
+
+<style scoped>
+.flicker-wrapper {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+}
+video {
+  max-width: 500px;
+  max-height: 900px;
+}
+</style>
